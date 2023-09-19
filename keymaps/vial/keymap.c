0c3d5d0e50b2e47b16b4e0d9550d2f52668f73a6
Astra Tanuki <105093560+sbkeebs@users.noreply.github.com>
2023-09-19 15:10:00 -0500

    Better labels and keymap

--- keymaps/vial/keymap.c
+++ keymaps/vial/keymap.c
@@ -1,4 +1,4 @@
-/* Copyright 2021 astra tanuki
+/* Copyright 2021 s8erdude
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -17,10 +17,10 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_ortho_grid(
-        KC_1, KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, KC_5,
-        KC_2, KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_6,
-        KC_3, KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMMA,KC_DOT,  KC_SLSH, KC_LSFT, KC_7,
-        KC_4, KC_LCTL, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_MINS, KC_EQL,  KC_8
+        KC_F1, KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,     KC_O,    KC_P,    KC_BSPC, KC_F7,
+        KC_F2, KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,     KC_L,    KC_SCLN, KC_QUOT, KC_F8,
+        KC_F3, KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMMA, KC_DOT,  KC_SLSH, KC_LSFT, KC_F9,
+        KC_F4, KC_F5,   KC_LCTL, KC_LGUI, KC_LALT, MO(1),   KC_ENT,  KC_SPC,  MO(2),   KC_RALT,  KC_RGUI, KC_MENU, KC_F6,   KC_F10
     )
 };
 
