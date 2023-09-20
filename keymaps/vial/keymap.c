7735761479dcf46c816100677246b4a814291733
sbkeebs <105093560+sbkeebs@users.noreply.github.com>
2023-09-20 10:51:12 -0400

    Update keymap.c
    haha forgot to change a name from copypastaing the keymap

--- keymaps/vial/keymap.c
+++ keymaps/vial/keymap.c
@@ -1,4 +1,4 @@
-/* Copyright 2021 s8erdude
+/* Copyright 2023 astra tanuki but dw about it
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
